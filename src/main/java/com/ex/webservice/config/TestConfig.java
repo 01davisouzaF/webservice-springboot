e4dc1aa4152024d88d10904c803b6a35869f68ee
01davisouzaF <luizdavi.freitasf@gmail.com>
2024-07-13 22:47:04 -0300

    Payment, one-to-one association

--- src/main/java/com/ex/webservice/config/TestConfig.java
+++ src/main/java/com/ex/webservice/config/TestConfig.java
@@ -3,16 +3,12 @@ package com.ex.webservice.config;
 import java.time.Instant;
 import java.util.Arrays;
 
+import com.ex.webservice.entities.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import com.ex.webservice.entities.Category;
-import com.ex.webservice.entities.Order;
-import com.ex.webservice.entities.OrderItem;
-import com.ex.webservice.entities.Product;
-import com.ex.webservice.entities.User;
 import com.ex.webservice.entities.enums.OrderStatus;
 import com.ex.webservice.repositories.CategoryRepository;
 import com.ex.webservice.repositories.OrderItemRepository;
@@ -81,6 +77,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 	
